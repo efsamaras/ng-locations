ec871b7a62f690d75c09b27fc3eb24cdd4cfcf72
Stratos Samaras <samarasstratos@hotmail.com>
2023-10-07 00:53:47 +0300

    Navigation bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MapComponent } from './components/map/map.component';
+import { TableComponent } from './components/table/table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: MapComponent },
+    { path: 'map', component: MapComponent },
+    { path: 'table', component: TableComponent },
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MapComponent } from './components/map/map.component';
+import { TableComponent } from './components/table/table.component';
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    declarations: [AppComponent, NavbarComponent, MapComponent, TableComponent],
+    imports: [BrowserModule, AppRoutingModule, MatListModule, MatToolbarModule],
     providers: [],
     bootstrap: [AppComponent],
 })
