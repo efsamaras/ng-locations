b83f2f94cf99d7490b8c28a6dc465cb68ef808c0
Stratos Samaras <samarasstratos@hotmail.com>
2023-10-09 02:44:11 +0300

    Click map marker

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { Observable } from 'rxjs';
 import { GoogleLocation, LocationsService } from '../../services/locations.service';
 import { GoogleApisService } from '../../services/google-apis.service';
+import { MapInfoWindow, MapMarker } from '@angular/google-maps';
 
 @Component({
     selector: 'app-map',
@@ -9,13 +10,16 @@ import { GoogleApisService } from '../../services/google-apis.service';
     styleUrls: ['./map.component.scss'],
 })
 export class MapComponent implements OnInit {
+    @ViewChild(MapInfoWindow, { static: false }) infoWindow: MapInfoWindow;
+    @ViewChildren(MapMarker) mapMarkers: QueryList<MapMarker>;
+
     apiLoaded: Observable<boolean>;
     options: google.maps.MapOptions = {
         center: { lat: 40, lng: 25 },
         zoom: 4,
     };
-
     locations: GoogleLocation[] = [];
+    mapInfoContent = '';
 
     constructor(
         private locationsService: LocationsService,
@@ -30,5 +34,11 @@ export class MapComponent implements OnInit {
         });
     }
 
-    onMarkerClick(location: GoogleLocation) {}
+    onMarkerClick(markerIndex: number, location: GoogleLocation) {
+        const mapMarker = this.mapMarkers.get(markerIndex);
+        if (mapMarker) {
+            this.mapInfoContent = location.name;
+            this.infoWindow.open(mapMarker);
+        }
+    }
 }
